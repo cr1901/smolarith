c4affcc97ba99b217a72e903af704d4e36d9c01d
William D. Jones <thor0505@comcast.net>
2024-02-15 03:53:38 -0500

    Greatly improve bench module.

--- src/smolarith/bench.py
+++ src/smolarith/bench.py
@@ -1,6 +1,9 @@
+import json
 import subprocess
-import re
+from string import Template
 
+from io import StringIO
+from amaranth import Elaboratable
 from amaranth.back import rtlil
 
 
@@ -8,39 +11,51 @@ class RunnerError(Exception):
     pass
 
 
-def stats(m):
-    raise NotImplementedError
+GENERIC_SCRIPT = Template("""
+${quiet} read_ilang << rtlil
+${rtlil_text}
+rtlil
+${quiet} hierarchy -check
+${quiet} proc
+${quiet} flatten
+${quiet} tribuf -logic
+${quiet} deminout
+${quiet} synth -run coarse
+${quiet} memory_map
+${quiet} opt -full
+${quiet} techmap -map +/techmap.v
+${quiet} opt -fast
+${quiet} dfflegalize -cell $$_DFF_P_ 0
+${quiet} abc -lut 4 -dress
+${quiet} clean -purge
+stat -json
+""")
 
-    # if args.action == "size":
-    # fragment = Fragment.get(design, platform)
-    # rtlil_text = rtlil.convert(fragment, name=name, ports=ports)
-    rtlil_text = rtlil.convert(m, ports=[m.a, m.b, m.o])
 
-    # Created from a combination of amaranth._toolchain.yosys and
-    # amaranth.back.verilog. Script comes from nextpnr-generic.
-    script = []
-    script.append("read_ilang <<rtlil\n{}\nrtlil".format(rtlil_text))
-    script.append("hierarchy -check")
-    script.append("proc")
-    script.append("flatten")
-    script.append("tribuf -logic")
-    script.append("deminout")
-    script.append("synth -run coarse")
-    script.append("memory_map")
-    script.append("opt -full")
-    script.append("techmap -map +/techmap.v")
-    script.append("opt -fast")
-    script.append("dfflegalize -cell $_DFF_P_ 0")
-    script.append("abc -lut 4 -dress")
-    script.append("clean -purge")
-    # if args.show:
-    script.append("show")
-    script.append("hierarchy -check")
-    script.append("stat")
+ICE40_SCRIPT = Template("""
+${quiet} read_ilang << rtlil
+${rtlil_text}
+rtlil
+${quiet} synth_ice40
+stat -json
+""")
 
-    stdin = "\n".join(script)
 
-    popen = subprocess.Popen(["yosys", "-"],
+ECP5_SCRIPT = Template("""
+${quiet} read_ilang << rtlil
+${rtlil_text}
+rtlil
+${quiet} synth_lattice -family ecp5
+stat -json
+""")
+
+
+def stats(m, script):
+    rtlil_text = rtlil.convert(m)
+
+    stdin = script.substitute(rtlil_text=rtlil_text, quiet="tee -q")
+
+    popen = subprocess.Popen(["yosys", "-Q", "-T", "-"],
                              stdin=subprocess.PIPE,
                              stdout=subprocess.PIPE,
                              stderr=subprocess.PIPE,
@@ -49,21 +64,50 @@ def stats(m):
     if popen.returncode:
         raise RunnerError(stderr.strip())
 
-    # if args.verbose:
-    #    print(stdout)
-    # else:
-    begin_re = re.compile(r"[\d.]+ Printing statistics.")
-    end_re = re.compile(r"End of script.")
-    capture = False
-    # begin_l = 0
-    # end_l = 0
+    # Find the start of the JSON from the stats command.
+    for i, l in enumerate(StringIO(stdout).readlines()):
+        if l[0] == "{":
+            break
 
-    for i, l in enumerate(stdout.split("\n")):
-        if begin_re.match(l):
-            capture = True
+    # Restart read since we can't really put back a line...
+    stdout = StringIO(stdout)
+    stdout.readlines(i)  # Drain non-JSON lines.
+    print(json.load(stdout))
 
-        if end_re.match(l):
-            capture = False
 
-        if capture:
-            print(l)
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    import argparse
+    import importlib
+
+    p = argparse.ArgumentParser(description="smolarith benchmarking program using yosys")  # noqa: E501
+    p.add_argument("-s", choices=("generic", "ice40", "ecp5"), default="generic", help="script to execute")  # noqa: E501
+    p.add_argument("-w", type=int, default=8, help="width of Amaranth component to test")  # noqa: E501
+    p.add_argument("import_path", help="relative import path (i.e. Amaranth component) in smolarith to benchmark (include leading '.')")  # noqa: E501
+
+    args = p.parse_args()
+
+    if args.s == "generic":
+        script = GENERIC_SCRIPT
+    elif args.s == "ice40":
+        script = ICE40_SCRIPT
+    elif args.s == "ecp5":
+        script = ECP5_SCRIPT
+    else:
+        assert False
+
+    try:
+        split_path = args.import_path.split(".")
+        implib_path = ".".join(split_path[:-1])
+        cls = getattr(importlib.import_module(implib_path, "smolarith"), split_path[-1])  # noqa: E501
+    except ImportError as e1:
+        raise ImportError("this doesn't look like an import from smolarith") from e1  # noqa: E501
+
+    m = cls(args.w)
+    if not isinstance(m, Elaboratable):
+        raise ValueError(f"{cls} does not look like an Elaboratable")
+
+    stats(m, script)
